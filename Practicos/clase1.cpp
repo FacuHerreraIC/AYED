193ba2ee9ec954caf6ab3a2ac6f9ca3d84b966d4
Facundo Herrera <facundo.herrera@mi.unc.edu.ar>
2025-08-05 19:24:38 -0300

    Ejercicios Clase 1 update

--- Practicos/clase1.cpp
+++ Practicos/clase1.cpp
@@ -2,4 +2,53 @@
 // Created by facuh on 8/5/25.
 //
 
-#include "clase1.h"
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void swap(int &a,int &b);
+
+int main(){
+    int x,y;
+    x = 55; y = -22;
+    cout <<"\n x = "<<x;
+    cout <<"\n y = "<<y;
+    cout <<"\n tamaño entero = "<<sizeof(int);
+    int *px,*py;
+    px = &x; py = &y;
+    cout <<"\n px = "<<px;
+    cout <<"\n py = "<<py;
+    cout <<"\n *px = "<<*px;
+    cout <<"\n *py = "<<*py;
+    *px=7; cout<<"\n *px = "<<*px<<" x ="<<x;
+    py=px; cout<<"\n *py = "<<*py<<" y="<<y;
+    swap(x,y);
+    cout <<"\n x = "<<x;
+    cout <<"\n y = "<<y;
+}
+//pasaje por referencia
+void swap(int &a,int &b){
+    int c;
+    cout<<"\n a = "<<a<<" b = "<<b;
+    c=a; a=b; b=c;
+    cout<<"\n a = "<<a<<" b = "<<b;
+}
+/*
+ *pasaje por puntero
+void swap(int *a,int *b){
+    int c;
+    cout<<"\n a = "<<*a<<" b = "<<*b;
+    c=*a; *a=*b; *b=c;
+    cout<<"\n a = "<<*a<<" b = "<<*b;
+}
+*/
+/*
+//intercambio de valores en caso int
+void swap(int a,int b){
+    int c;
+    cout<<"\n a = "<<a<<" b = "<<b;
+    c=a; a=b; b=c;
+    cout<<"\n a = "<<a<<" b = "<<b;
+}
+*/
